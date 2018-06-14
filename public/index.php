63aae5e1d99735de93475b028aeeb43bce1990dc
barracuda75 <wilsai.mede@hetic.net>
2018-06-14 14:51:07 +0200

    ajout d'une entrée dans la base de donnée

--- public/index.php
+++ public/index.php
@@ -1,8 +1,7 @@
 <?php
 session_start();
 
-define( 'ROOT', str_replace( 'public/index.php', '', $_SERVER['SCRIPT_FILENAME'] ) );
-
+define( 'ROOT', dirname(__DIR__) . '/' );
 
 require_once ROOT . 'app/core/App.php';
 require_once ROOT . 'app/core/Controller.php';
