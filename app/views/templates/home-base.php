9b7eafb098ad961d67166ff9843aebbb3b43cea3
Leo <leopold.von-der-weid@hetic.net>
2018-06-15 06:54:11 +0200

    Séparation des section en fichier tempalte

--- app/views/templates/home-base.php
+++ app/views/templates/home-base.php
@@ -20,33 +20,10 @@
         <?php include(ROOT . 'app/views/templates/home-topbar.php');?>
 
 
-      <div class="home__content">
-        <div class="home__content__wraper">
-          <h1 class="home__content__title">Bienvenue Jane</h1>
-          <h2 class="home__content__subtitle">Personnalisez votre espace Modulab 📊</h2>
-          <div class="home__content__wiget">
-            <p>+ Ajouter un widget</p>
-          </div>
-        </div>
 
-        <!-- ================= Profil user ================== -->
-        <div class="home__content__wraper">
-          <h1 class="home__content__title">Votre profil</h1>
-          <div class="home__content__profil__wraper">
-            <div class="home__content__profil__info">
-              <h3 class="home__content__profil__info__name">Jane Doe</h3>
-              <p class="home__content__profil__info__job">Strategic Planner @Ethereon</p>
-              <p class="home__content__profil__info__phone">06 89 01 92 35</p>
-              <p class="home__content__profil__info__email">Jane.doe@ethereon.net</p>
-              <div class="home__content__profil__info__modify">
+        <?php include(ROOT . 'app/views/templates/home-content.php');?>
+        <?php //include(ROOT . 'app/views/templates/home-profil.php');?>
 
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <!-- ================= Profil user ================== -->
-        <?php include(ROOT . 'app/views/templates/home-profil.php');?>
       </div>
     </div>
   </div>
