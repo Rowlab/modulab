7d2c514d651e144b0065ad34884da87c03e28307
Leo <leopold.von-der-weid@hetic.net>
2018-06-15 06:33:44 +0200

    Ajout page profil utilisateur

--- app/views/templates/home-base.php
+++ app/views/templates/home-base.php
@@ -29,7 +29,23 @@
           </div>
         </div>
 
+        <!-- ================= Profil user ================== -->
+        <div class="home__content__wraper">
+          <h1 class="home__content__title">Votre profil</h1>
+          <div class="home__content__profil__wraper">
+            <div class="home__content__profil__info">
+              <h3 class="home__content__profil__info__name">Jane Doe</h3>
+              <p class="home__content__profil__info__job">Strategic Planner @Ethereon</p>
+              <p class="home__content__profil__info__phone">06 89 01 92 35</p>
+              <p class="home__content__profil__info__email">Jane.doe@ethereon.net</p>
+              <div class="home__content__profil__info__modify">
 
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- ================= Profil user ================== -->
         <?php include(ROOT . 'app/views/templates/home-profil.php');?>
       </div>
     </div>
